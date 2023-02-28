4bade5e91968a63c14447747bc514399b1409ab4
Md. Nazirul Mobin Ahamed <110886615+AntoAhamed@users.noreply.github.com>
2023-02-28 20:33:17 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".btn");
+
+button.addEventListener('click', () => {
+    alert("Ypu can't submit this form right now because this website is under construction.");
+});
